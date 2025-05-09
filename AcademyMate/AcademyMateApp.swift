3d28c228c8d3ab95fd4b5e76ba52250f9fd5eaa4
Arin <arinjuansari@gmail.com>
2025-05-09 09:10:04 +0700

    Initial Commit

---
+++ AcademyMate/AcademyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyMateApp.swift
+//  AcademyMate
+//
+//  Created by Arin Juan Sari on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AcademyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
